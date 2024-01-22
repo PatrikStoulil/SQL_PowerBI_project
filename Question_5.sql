7736ea7e63919e6bd351b2ddf341657f22b83cef
Patrik Stoulil <patriksstoulil@seznam.cz>
2024-01-22 19:11:09 +0100

    connection of secondary table to WITH clausule to be able to compare year_change of GDP

--- Question_5.sql
+++ Question_5.sql
@@ -28,3 +28,28 @@ WITH salary_and_comodities_year_change AS (
 			payroll_year) b
 	WHERE
 		a.payroll_year = b.payroll_year)
+SELECT
+		tps1.year,
+		tps1.GDP,
+		tps2.year as previous_year,
+		tps2.GDP as previous_year_GDP,
+		round((tps1.GDP-tps2.GDP)/ tps2.GDP * 100,2) as GDP_growth,
+		sacyc.salary_year_Change,
+		sacyc.commodities_price_year_change,
+		sacyc2.payroll_year as following_year,
+		sacyc2.salary_year_Change as following_year_salary_change,
+		sacyc2.commodities_price_year_change as following_year_commodities_price_change
+	FROM
+		t_Patrik_Stoulil_project_SQL_secondary_final tps1
+	LEFT JOIN t_Patrik_Stoulil_project_SQL_secondary_final tps2
+ON
+		tps1.`year` = tps2.`year` + 1
+		AND tps1.country = tps2.country
+	LEFT JOIN salary_and_comodities_year_change sacyc
+ON
+		tps1.`year` = sacyc.payroll_year
+	LEFT JOIN salary_and_comodities_year_change sacyc2
+ON
+		tps1.`year` = sacyc2.payroll_year - 1
+	WHERE
+		tps1.country = 'Czech Republic'
